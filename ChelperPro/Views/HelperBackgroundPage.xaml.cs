e4ceb22f23ae39fe5bb6cb3ff847fec3bb1315f8
bo.chen@crosshelper.com <bo.chen@crosshelper.com>
2019-04-30 15:21:05 -0700

    background done

--- ChelperPro/Views/HelperBackgroundPage.xaml.cs
+++ ChelperPro/Views/HelperBackgroundPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace ChelperPro.Views
@@ -15,9 +16,46 @@ namespace ChelperPro.Views
         {
             Navigation.PopToRootAsync(false);
         }
+
+        public IList HomeLandItems { get; }
+        //Homeland
+        public ObservableCollection<HomeLandPickerItem> HomeLandItemsSource { get; } = new ObservableCollection<HomeLandPickerItem>();
+        public ObservableCollection<HomeLandPickerItem> HomeLandSelectedItems { get; set; } = new ObservableCollection<HomeLandPickerItem>();
+
+
         public HelperBackgroundPage()
         {
             InitializeComponent();
+            List<string> list = new List<string>
+            {
+                "Chinese",
+                "English",
+                "Spanish",
+                "Korean",
+                "Japanese",
+                "French",
+                "German",
+                "Thai"
+            };
+            HomeLandItems = list;
+
+            //出生地 pick homeland
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "China" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "United States" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "France" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "Germany" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "Japan" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "South Korea" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "Spain" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "Mexico" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "Brazil" });
+            HomeLandItemsSource.Add(new HomeLandPickerItem { Name = "Thailand" });
+
+            BindingContext = this;
+        }
+        public class HomeLandPickerItem
+        {
+            public string Name { get; set; }
         }
     }
 }
