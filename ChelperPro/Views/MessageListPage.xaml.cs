967ac9d7e0a63725293746b63bde127bd30715a0
William Liang <williamliang30@gmail.com>
2019-05-23 19:15:37 -0700

    123

---
+++ ChelperPro/Views/MessageListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ChelperPro.Views
+{
+    public partial class MessageListPage : ContentPage
+    {
+        public MessageListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
