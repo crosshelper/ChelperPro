c335b2943c2c79a0e8ad7a57895c6b926d28a5e6
William Liang <williamliang30@gmail.com>
2019-05-30 20:41:20 -0700

    4124

--- ChelperPro/App.xaml.cs
+++ ChelperPro/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using ChelperPro.Views;
 using Stripe;
+using SendBird;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace ChelperPro
 {
@@ -15,7 +19,8 @@ namespace ChelperPro
         protected override void OnStart()
         {
             // Handle when your app starts
-            //SendBirdClient.Init(ChelperPro.Properties.Resources.APP_ID);
+
+            SendBirdClient.Init("B0F51364-A66F-4EF8-B6AA-0A61629E45C7");//ChelperPro.Properties.Resources.APP_ID);
             StripeConfiguration.SetApiKey("sk_live_XXXXXXXXXXXXXXX");
         }
 
