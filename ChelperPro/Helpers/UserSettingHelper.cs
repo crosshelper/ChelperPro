3a6ac5098d7d12a6da3d1abdbc9dcaa3b318b36a
william <william.liang@crosshelper.com>
2019-05-31 18:51:43 -0700

    nn

--- ChelperPro/Helpers/UserSettingHelper.cs
+++ ChelperPro/Helpers/UserSettingHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ChelperPro.Models;
 using MySql.Data.MySqlClient;
 
@@ -44,6 +45,72 @@ namespace ChelperPro.Helpers
             }
         }
 
+        internal void UpdateZipCode(string zipCode1, string zipCode2, string zipCode3)
+        {
+            //建立数据库连接
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {   //建立连接，打开数据库
+                conn.Open();
+                string sqlstr =
+                    "UPDATE HelperInfo SET " +
+                    "ServiceZip1 = @para2, " +
+                    "ServiceZip2 = @para3, " +
+                    "ServiceZip3 = @para4" +
+                    " WHERE Uid = @para1";
+                MySqlCommand cmd = new MySqlCommand(sqlstr, conn);
+                //通过设置参数的形式给SQL 语句串值
+                cmd.Parameters.AddWithValue("para1", Settings.UserId);
+                cmd.Parameters.AddWithValue("para2", zipCode1);
+                cmd.Parameters.AddWithValue("para3", zipCode2);
+                cmd.Parameters.AddWithValue("para4", zipCode3);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();   //关闭连接              
+            }
+        }
+
+        internal List<string> GetZipCodeByID(string userId)
+        {
+            List<string> ziplist = new List<string>();
+            //并没有建立数据库连接
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {   //建立连接，打开数据库
+                conn.Open();
+                string sqlstr =
+                "SELECT ServiceZip1,ServiceZip2,ServiceZip3 FROM HelperInfo WHERE HelperInfo.Uid = @para1";
+
+                MySqlCommand cmd = new MySqlCommand(sqlstr, conn);
+                //通过设置参数的形式给SQL 语句串值
+                cmd.Parameters.AddWithValue("para1", userId);
+                //cmd.Parameters.AddWithValue("para2", password);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ziplist.Add(reader.GetString(0));
+                    ziplist.Add(reader.GetString(1));
+                    ziplist.Add(reader.GetString(2));
+                }
+                return ziplist;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return ziplist;
+            }
+            finally
+            {
+                conn.Close();   //关闭连接              
+            }
+        }
 
         internal void UpdateUac(Uac ac)
         {
--- ChelperPro/Views/ServiceAreaPage.xaml.cs
+++ ChelperPro/Views/ServiceAreaPage.xaml.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using ChelperPro.Helpers;
 using Xamarin.Forms;
 
 namespace ChelperPro.Views
 {
     public partial class ServiceAreaPage : ContentPage
     {
+        UserSettingHelper ush = new UserSettingHelper();
         public ServiceAreaPage()
         {
             InitializeComponent();
+            List<string> ZipCodeslist = ush.GetZipCodeByID(Settings.UserId);
+            if(ZipCodeslist.Count>0)
+            {
+                ZipCode1 = ZipCodeslist[0];
+                ZipCode2 = ZipCodeslist[1];
+                ZipCode3 = ZipCodeslist[2];
+            }
         }
+        public string ZipCode1 { get; set; }
+        public string ZipCode2 { get; set; }
+        public string ZipCode3 { get; set; }
+
         void Handle_Save(object sender, System.EventArgs e)
         {
+            if(!string.IsNullOrEmpty(ZipCode1) && !string.IsNullOrEmpty(ZipCode2) && !string.IsNullOrEmpty(ZipCode3))
+            ush.UpdateZipCode(ZipCode1,ZipCode2,ZipCode3);
             Navigation.PopAsync();
+
         }
     }
 }
