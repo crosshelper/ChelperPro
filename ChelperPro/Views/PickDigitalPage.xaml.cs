d5caae02f1b2451e0a874c77dd0c3e6055c696f1
bochen <bo.chen@crosshelper.com>
2019-08-27 23:28:12 -0700

    add

---
+++ ChelperPro/Views/PickDigitalPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ChelperPro.Helpers;
+using ChelperPro.Models;
+using Xamarin.Forms;
+
+namespace ChelperPro.Views
+{
+    public partial class PickDigitalPage : ContentPage
+    {
+        void Handle_location(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new CurrentLocationPage());
+        }
+        public void OnMore(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Report to us", "Please send email to 'supports@cycbis.com' with the screenshot of the problem, thanks!", "Got it"); //"More Context Action", mi.CommandParameter + " more context action", "OK");
+        }
+
+        public List<TopicInfoLabel> Topics { get; private set; }
+        TopicInfoHelper tih = new TopicInfoHelper();
+        UserInfoHelper uih = new UserInfoHelper();
+
+        public PickDigitalPage()
+        {
+            InitializeComponent();
+            var _currentZipCodeList = uih.GetMyServiceArea();
+            Topics = tih.GetMyTopicList(_currentZipCodeList); //_currentZipCode);
+            BindingContext = this;
+        }
+        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            TopicInfoLabel topicInfoLabel = e.Item as TopicInfoLabel;
+            ((ListView)sender).SelectedItem = null;
+            Navigation.PushModalAsync(new NavigationPage(new NewcomerProblemPage(topicInfoLabel)));
+        }
+
+        void Handle_Toggled(object sender, ToggledEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
